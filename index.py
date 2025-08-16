b6774d3830e0353e3a92b17d0b36e74918800605
Quentin Vespero <quentinvespero@gmail.com>
2025-08-17 01:59:14 +0200

    testing voxtral transcription api

--- index.py
+++ index.py
@@ -12,16 +12,21 @@ model = "voxtral-mini-latest"
 # initialise mistral client
 client = Mistral(api_key=api_key)
 
-# getting transcription
+# getting transcription from local file
 with open('./s10e43_trimmed_benchmark.mp3', 'rb') as file:
     transcription_response = client.audio.transcriptions.complete(
+        model=model,
+        file={'content': file ,'file_name':'./s10e43_trimmed_benchmark.mp3'},
         timestamp_granularities=['segment'],
         language='fr',
-        model=model,
-        file={
-            'content': file ,
-            'file_name':'./s10e43_trimmed_benchmark.mp3'
-            },
 )
 
-print(transcription_response.language, transcription_response.segments)
+# getting transcription from test file
+# transcription_response = client.audio.transcriptions.complete(
+#     model=model,
+#     file_url="https://docs.mistral.ai/audio/obama.mp3",
+#     timestamp_granularities=['segment']
+# )
+
+print('---------- segment ----------',transcription_response.segments)
+print('---------- text ----------',transcription_response.text)
