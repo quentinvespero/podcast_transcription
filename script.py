0012cf86dd944bade10d98251aef09a9189b138e
Quentin Vespero <quentinvespero@gmail.com>
2025-09-14 23:43:33 +0200

    create final script

---
+++ script.py
@@ -0,0 +1,10 @@
+# config
+
+db_path = '.data/podcast.db'
+input_files__dir = '.input_files/'
+transcription_model = 'large-v3'
+transcription_model_path = './whisper.cpp/models'
+
+# transcription
+
+# main
